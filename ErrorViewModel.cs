93e3ad2e1edde1ec846d16bb0bec15b49ad159e1
Krish Pradeep <krish02062006@gmail.com>
2026-03-12 09:50:08 +0530

    Delete ErrorViewModel.cs

--- ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace register.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
